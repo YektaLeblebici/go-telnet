050dffc82f34b801e8d196b3b7d12542c96ffa40
Yekta Leblebici <yekta@iamyekta.com>
2020-12-28 21:58:05 +0100

    inject net.Conn to context

--- context.go
+++ context.go
@@ -1,17 +1,18 @@
 package telnet
 
+import "net"
 
 type Context interface {
 	Logger() Logger
 
 	InjectLogger(Logger) Context
+	InjectConn(net.Conn) Context
 }
-
 
 type internalContext struct {
 	logger Logger
+	conn   net.Conn
 }
-
 
 func NewContext() Context {
 	ctx := internalContext{}
@@ -19,13 +20,18 @@ func NewContext() Context {
 	return &ctx
 }
 
-
 func (ctx *internalContext) Logger() Logger {
 	return ctx.logger
 }
 
 func (ctx *internalContext) InjectLogger(logger Logger) Context {
 	ctx.logger = logger
+
+	return ctx
+}
+
+func (ctx *internalContext) InjectConn(c net.Conn) Context {
+	ctx.conn = c
 
 	return ctx
 }
